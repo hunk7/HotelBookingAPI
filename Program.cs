bbc44d491b1333ff2850bb989163d343a3cbd7b8
yxwb <yxwb@WIACPVDD-002850>
2024-12-07 17:25:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using HotelBookingAPI.Connection;
+using HotelBookingAPI.Repository;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
+// Initialize Serilog from appsettings.json
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<SqlConnectionFactory>();
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<RoomTypeRepository>();
+builder.Services.AddScoped<RoomRepository>();
+builder.Services.AddScoped<AmenityRepository>();
+builder.Services.AddScoped<RoomAmenityRepository>();
+builder.Services.AddScoped<HotelSearchRepository>();
+builder.Services.AddScoped<ReservationRepository>();
+builder.Services.AddScoped<CancellationRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
